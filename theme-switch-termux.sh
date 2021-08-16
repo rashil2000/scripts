c4cf6a5e837743731f78d2823139f7d6ecece3fa
Rashil Gandhi <rashil2000@gmail.com>
2021-08-16 23:47:03 +0530

    Add gitconfig

--- theme-switch-termux.sh
+++ theme-switch-termux.sh
@@ -1,18 +1,16 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 rm ~/.termux/colors.properties
-cd ~/projects/scripts/ts
+cd ~/GitHub/rashil2000/scripts/Nice
 theme=$(cat current)
 if [ $theme == "dark" ] ; then
   cp one-light ~/.termux/colors.properties
   echo 'light' | tee current > /dev/null
   termux-reload-settings
-  sed -i -e 's/\"dark\"/\"light\"/g' ~/.config/nvim/init.vim
-  sed -i -e 's/\"ayu_dark\"/\"ayu_light\"/g' ~/.config/nvim/init.vim
+  sed -i -e 's/decorations\ calochortus-lyallii/decorations\ hoopoe/g' ~/.config/git/config
 else
   cp default ~/.termux/colors.properties
   echo 'dark' | tee current > /dev/null
   termux-reload-settings
-  sed -i -e 's/\"light\"/\"dark\"/g' ~/.config/nvim/init.vim
-  sed -i -e 's/\"ayu_light\"/\"ayu_dark\"/g' ~/.config/nvim/init.vim
+  sed -i -e 's/decorations\ hoopoe/decorations\ calochortus-lyallii/g' ~/.config/git/config
 fi
