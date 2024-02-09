caf0cccc41e90344af1a9bb63d43b843f1c3f11a
Rashil Gandhi <46838874+rashil2000@users.noreply.github.com>
2024-02-09 12:47:11 +0530

    Update theme-switch-termux.sh

--- theme-switch-termux.sh
+++ theme-switch-termux.sh
@@ -6,11 +6,11 @@ rm colors.properties
 if grep -Fxq "use-black-ui = true" termux.properties ; then
   cp google-light.properties colors.properties
   sed -i -e 's/use-black-ui\ =\ true/use-black-ui\ =\ false/g' termux.properties
-  termux-reload-settings
 else
   cp google-dark.properties colors.properties
   sed -i -e 's/use-black-ui\ =\ false/use-black-ui\ =\ true/g' termux.properties
-  termux-reload-settings
 fi
+
+termux-reload-settings
 
 popd
